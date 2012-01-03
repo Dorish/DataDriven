df895226027f812b46a1c65cdef002c9e53ad664
darryl brown <darryl.brown@emerson.com>
2012-01-03 00:52:50 -0500

    Rename IS-WEB Watir test scripts to include
    Add Testlog numbers to IS-WEB(SNMPv3)web scripts
    Reference Rally REQ6193

---
+++ driver/web/700.150.20.110-cfg_sms_messag_web.rb
@@ -0,0 +1,137 @@
+=begin rdoc
+*Revisions*
+  | Change                                               | Name        | Date  |
+
+*Test_Script_Name*
+  sms_messag
+
+*Test_Case_Number*
+  700.150.20.110
+
+*Description*
+  Validate the SMS Messaging Information configuration
+     - Positive
+     - Negative
+     - Boundary
+
+*Variable_Definitions*
+    s = test start time
+    f = test finish time
+    e = test elapsed time
+    roe = row number in controller spreadsheet
+    excel = nested array that contains an instance of excel and driver parameters
+    ss = spreadsheet
+    wb = workbook
+    ws = worksheet
+    dvr_ss = driver spreadsheet
+    rows = number of rows in spreadsheet to execute
+    site = url/ip address of card being tested
+    name = user name for login
+    pswd = password for login
+
+=end
+
+#Launch the Configure SMS ruby script
+#Add library file to the path
+$:.unshift File.dirname(__FILE__).chomp('driver/web')<<'lib' # add library to path
+s = Time.now
+require 'generic'
+require 'watir/process' 
+
+begin 
+  puts" \n Executing: #{(__FILE__)}\n\n" # print current filename
+  g = Generic.new
+  roe = ARGV[1].to_i
+  #Open up a new excel instance and save it with the timestamp.
+  #Open the IE browser
+  #Collect the support page information and save it in the time stamped spreadsheet.
+  excel = g.setup(__FILE__)
+  wb,ws = excel[0][1,2]
+  rows = excel[1][1] 
+
+  #TODO - these ss formatting commands will be move to setup method in the future
+  ws.Columns("A:B").HorizontalAlignment = 2                   # Left Align text
+  ws.Rows("2:#{rows + 1}").RowHeight = 12.75                  # Set row height
+
+  $ie.speed = :zippy
+  g.config.click
+  $ie.maximize  
+  #Click the Configure SMS link on the left side of window
+  #Login if not called from controller
+  g.logn_chk(g.sms,excel[1])
+
+  #Enable sms on msging page first.
+  g.wrt_checkbox(g.sms_msg, 'set', g.msging, g.sms)
+  row = 1
+  fail = 0
+  while(row <= rows)
+    puts "Test step #{row}"
+    row +=1 # add 1 to row as execution starts at drvr_ss row 2
+  
+
+    # ***** write sms fields ****
+    g.edit.click
+    g.sms_from.set(ws.Range("k#{row}")['Value'].to_s)           # From
+    g.sms_to.set(ws.Range("l#{row}")['Value'].to_s)             # To
+    subj_type = ws.Range("m#{row}")['Value'].to_i               # Subject Type
+    if subj_type == 0
+      g.sms_subjecttype(0).set
+    else
+      g.sms_subjecttype(1).set
+      g.sms_custsubj.set(ws.Range("n#{row}")['Value'].to_s)     # Custom Subject
+    end
+    g.sms_srvr.set(ws.Range("o#{row}")['Value'].to_s)           # Server
+    g.sms_port.set(ws.Range("p#{row}")['Value'].to_s)           # Port
+
+    # popup expected?
+    pop = ws.Range("af#{row}")['Value'].to_s
+    if (pop == "res")                                           # Reset
+      ws.Range("bi#{row}")['Value'] = g.invChar(pop)    # popup text
+    end
+    if (pop == "can")                                           # Cancel
+      ws.Range("bi#{row}")['Value'] = g.res_can(pop)
+    end
+    #TODO this will need to be cleaned up. Simple reset should not use "pop" variable
+    if (pop == "reset")                                         # Reset only (no popup)
+      g.reset.click_no_wait
+      g.jsClick("OK")
+    end
+    if (pop == "no")                                            # save only if no popup
+      g.save.click
+    end
+
+
+     #**** read sms fields ****
+     sleep 1 #sleep 1 is need to make sure no intermittent failing
+    ws.Range("bc#{row}")['Value'] = g.sms_from.value            # From
+    ws.Range("bd#{row}")['Value'] = g.sms_to.value              # To
+    if g.sms_subjecttype(0).checked? == true                    # Subject Type
+      ws.Range("be#{row}")['Value'] = "0"
+    else
+      ws.Range("be#{row}")['Value'] = "1"
+      ws.Range("bf#{row}")['Value'] = g.sms_custsubj.value      # Custom Subject
+    end
+    ws.Range("bg#{row}")['Value'] = g.sms_srvr.value            # Server
+    ws.Range("bh#{row}")['Value'] = g.sms_port.value            # Port
+
+    wb.Save
+  end
+    
+  f = Time.now  #finish time
+  #Capture error if any in the script
+rescue Exception => e
+  f = Time.now  #finish time 
+  puts" \n\n **********\n\n #{$@ } \n\n #{e} \n\n ***"
+  error_present=$@.to_s
+
+ensure #this section is executed even if script goes in error
+  #Disable sms on msging page.
+  g.wrt_checkbox(g.sms_msg, 'clear', g.msging, g.sms)
+    # If roe > 0, script is called from controller
+    # If roe = 0, script is being ran independently
+    #Close and save the spreadsheet and thes web browser.
+    g.tear_down_d(excel[0],s,f,roe,error_present)
+    if roe == 0
+      $ie.close
+    end
+end
---
+++ driver/web/700.160.20.110-cfg_custm_messag_web.rb
@@ -0,0 +1,209 @@
+=begin rdoc
+*Revisions*
+  | Change                                               | Name        | Date  |
+
+*Test_Script_Name*
+  custm_messag
+
+*Test_Case_Number*
+  700.160.20.110
+
+*Description*
+  Validate the Customize Messaging Information configuration
+     Select
+     - Deselect
+     - Reset Ok and Reset Cancel
+
+*Variable_Definitions*
+    s = test start time
+    f = test finish time
+    e = test elapsed time
+    roe = row number in controller spreadsheet
+    excel = nested array that contains an instance of excel and driver parameters
+    ss = spreadsheet
+    wb = workbook
+    ws = worksheet
+    dvr_ss = driver spreadsheet
+    rows = number of rows in spreadsheet to execute
+    site = url/ip address of card being tested
+    name = user name for login
+    pswd = password for login
+
+=end
+
+#Launch the Customize Messaging ruby script
+#Add library file to the path
+$:.unshift File.dirname(__FILE__).chomp('driver/web')<<'lib' # add library to path
+s = Time.now
+require 'generic'
+require 'watir/process' 
+
+begin 
+  puts" \n Executing: #{(__FILE__)}\n\n" # print current filename
+  g = Generic.new
+  roe = ARGV[1].to_i
+  #Open up a new excel instance and save it with the timestamp.
+  #Open the IE browser
+  #Collect the support page information and save it in the time stamped spreadsheet.
+  excel = g.setup(__FILE__)
+  wb,ws = excel[0][1,2]
+  rows = excel[1][1] 
+
+  $ie.speed = :zippy
+  #Navigate to the 'Configure?tab
+  g.config.click
+  $ie.maximize  
+  #Click the Customize Messaging link on the left side of widow
+  #Login if not called from controller
+  g.logn_chk(g.custmsg,excel[1])
+  
+  row = 1
+  fail = 0
+  while(row <= rows)
+    puts "Test step #{row}"
+    row +=1 # add 1 to row as execution starts at drvr_ss row 2
+    begin
+    puts"Start of Normal loop"
+    sleep 3
+    g.edit.click
+
+    # Write customize email and sms checkboxes
+    flag = ws.Range("u#{row}")['Value'].to_s
+    puts "The flag is #{flag}"
+    case flag
+      when 'email'
+        g.wrt_checkbox(g.email_addr, ws.Range("k#{row}")['Value'])
+        g.wrt_checkbox(g.email_evtdesc, ws.Range("l#{row}")['Value'])
+        g.wrt_checkbox(g.email_name, ws.Range("m#{row}")['Value'])
+        g.wrt_checkbox(g.email_cont, ws.Range("n#{row}")['Value'])
+        g.wrt_checkbox(g.email_loc, ws.Range("o#{row}")['Value'])
+        g.wrt_checkbox(g.email_desc, ws.Range("p#{row}")['Value'])
+        g.wrt_checkbox(g.email_weblnk, ws.Range("q#{row}")['Value'])
+
+        g.wrt_checkbox(g.email_consol, ws.Range("r#{row}")['Value'])
+        if g.checkbox(g.email_consol)=='set'
+          g.email_consoltime.set(ws.Range("s#{row}")['Value'].to_s)
+          g.email_consolevt.set(ws.Range("t#{row}")['Value'].to_s)
+        end
+    
+      when 'sms'
+        g.wrt_checkbox(g.sms_addr, ws.Range("k#{row}")['Value'])
+        g.wrt_checkbox(g.sms_evtdesc, ws.Range("l#{row}")['Value'])
+        g.wrt_checkbox(g.sms_name, ws.Range("m#{row}")['Value'])
+        g.wrt_checkbox(g.sms_cont, ws.Range("n#{row}")['Value'])
+        g.wrt_checkbox(g.sms_loc, ws.Range("o#{row}")['Value'])
+        g.wrt_checkbox(g.sms_desc, ws.Range("p#{row}")['Value'])
+        g.wrt_checkbox(g.sms_weblnk, ws.Range("q#{row}")['Value'])
+
+        g.wrt_checkbox(g.sms_consol, ws.Range("r#{row}")['Value'])
+        if g.checkbox(g.sms_consol) == 'set'
+          g.sms_consoltime.set(ws.Range("s#{row}")['Value'].to_s)
+          g.sms_consolevt.set(ws.Range("t#{row}")['Value'].to_s)
+        end
+    end
+    
+    
+    #Is there a popup expected? 
+    pop = ws.Range("af#{row}")['Value'].to_s 
+    puts "  pop_up value = #{pop}" unless pop == 'no'
+    #sleep 1
+
+    #If popup, handle with reset OK or reset Cancel to continue
+    if (pop == "res")
+      popup_txt  = g.invChar(pop)
+      puts "Pop-Up text is #{popup_txt}"
+      ws.Range("bk#{row}")['Value'] = popup_txt
+    end
+    
+    if (pop == "can")
+      ws.Range("bk#{row}")['Value'] = g.res_can(pop)
+    end
+    
+ #If reset Cancel, do not save
+    if (pop == "no")
+      g.save.click_no_wait
+      g.jsClick('OK')
+    end
+ 
+    #read Email and SMS Checkbox value
+    sleep 1
+    g.edit.click
+    
+    case flag
+      when 'email'
+        ws.Range("bc#{row}")['Value'] = g.checkbox(g.email_addr)
+        ws.Range("bd#{row}")['Value'] = g.checkbox(g.email_evtdesc)
+        ws.Range("be#{row}")['Value'] = g.checkbox(g.email_name)
+        ws.Range("bf#{row}")['Value'] = g.checkbox(g.email_cont)
+        ws.Range("bg#{row}")['Value'] = g.checkbox(g.email_loc)
+        ws.Range("bh#{row}")['Value'] = g.checkbox(g.email_desc)
+        ws.Range("bi#{row}")['Value'] = g.checkbox(g.email_weblnk)
+        ws.Range("bj#{row}")['Value'] = g.checkbox(g.email_consol)
+        ws.Range("bl#{row}")['Value'] = g.email_consoltime.value
+        ws.Range("bm#{row}")['Value'] = g.email_consolevt.value 
+    
+      when 'sms'
+        ws.Range("bc#{row}")['Value'] = g.checkbox(g.sms_addr)
+        ws.Range("bd#{row}")['Value'] = g.checkbox(g.sms_evtdesc)
+        ws.Range("be#{row}")['Value'] = g.checkbox(g.sms_name)
+        ws.Range("bf#{row}")['Value'] = g.checkbox(g.sms_cont)
+        ws.Range("bg#{row}")['Value'] = g.checkbox(g.sms_loc)
+        ws.Range("bh#{row}")['Value'] = g.checkbox(g.sms_desc)
+        ws.Range("bi#{row}")['Value'] = g.checkbox(g.sms_weblnk)
+        ws.Range("bj#{row}")['Value'] = g.checkbox(g.sms_consol)
+        ws.Range("bl#{row}")['Value'] = g.sms_consoltime.value
+        ws.Range("bm#{row}")['Value'] = g.sms_consolevt.value 
+    
+    end
+    
+    g.save.click_no_wait
+    g.jsClick('OK')
+    wb.Save
+    
+    rescue
+    fail +=1
+    puts "Error - #{fail} of #{row}"
+    Watir.autoit.Send('{F5}')
+    puts"********** rescue ***"
+    sleep 2
+    
+    begin
+      sleep 5 # wait for the previous click_no_wait process to time out before starting another 
+      save.click_no_wait
+      jsClick( $ie, "OK")
+    rescue
+      puts "**********no 'save' popup in rescue***"
+    end
+    
+    g.config.click
+    puts"Clicked on Configure Tab"
+    
+    begin
+      g.custmsg.click_no_wait
+      g.jsClick('OK')
+      puts"**********email popup ***"
+    rescue
+      puts"**********return-1 to normal loop***"
+    end
+    puts"**********return-2 to normal loop***"
+    row = row - 1
+    next
+    end
+  end
+
+  f = Time.now  #finish time
+#Capture error if any in the script  
+rescue Exception => e
+  f = Time.now  #finish time 
+  puts" \n\n **********\n\n #{$@ } \n\n #{e} \n\n ***"
+  error_present=$@.to_s
+
+ensure #this section is executed even if script goes in error
+    # If roe > 0, script is called from controller
+    # If roe = 0, script is being ran independently
+    #Close and save the spreadsheet and thes web browser.
+    g.tear_down_d(excel[0],s,f,roe,error_present)
+    if roe == 0
+      $ie.close
+    end
+end
