100eac914110691db1261f88bcbce43ec9bc0e7b
Dorish Yu <yu.xiaohui.Dorish@emerson.com>
2012-01-06 15:22:45 +0800

    Fix script tbl_managementprotocol_web, reference DEF3978
    Add a local method hidden_table_info to handle this special
    situation.
    hidden_table_info is based on Generic.rb-> table_info().
    
    IS-WEBCARD_HID9_5.200.0_059101; Watir 1.7.0

--- driver/web/700.090.10.110-tbl_managementprotocol_web.rb
+++ driver/web/700.090.10.110-tbl_managementprotocol_web.rb
@@ -32,6 +32,31 @@ $:.unshift File.dirname(__FILE__).chomp('driver/web')<<'lib' # add library to pa
 s = Time.now
 require 'generic'
 
+# - used for configure information table scripts
+def hidden_table_info(g,start,_end,col,idx,ws)
+  # iterate through all rows and columns
+  pag_row = start # pag_row is the row number of web page table
+  while (start <= _end)
+    j = 1
+    while (j <= col)
+      case j
+      when 1
+        while g.param_descr(idx,pag_row,j).visible? ==false # skip the rows if invisible
+          pag_row += 1
+        end
+        parameter = g.param_descr(idx,pag_row,j).text
+        ws.Range("bc#{start+1}")['Value'] =  parameter
+      when 2
+        description = g.param_descr(idx,pag_row,j).text
+        ws.Range("bd#{start+1}")['Value'] = description
+      end
+      j += 1
+    end
+    start += 1
+    pag_row += 1
+  end
+end
+
 begin
   puts" \n Executing: #{(__FILE__)}\n\n" # show current filename
   g = Generic.new
@@ -42,7 +67,7 @@ begin
   g.config.click
   g.logn_chk(g.mgtprot,excel[1])
   
-  g.table_info(1,2,2,2,ws)
+  hidden_table_info(g,1,3,2,2,ws)
   
 rescue Exception => e
   puts" \n\n **********\n\n #{$@ } \n\n #{e} \n\n ***"
