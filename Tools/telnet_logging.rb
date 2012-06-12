c3e6f25f461e4ab385e897f7cedf7bedb592c450
Mary Ma <xiaoli.ma@emerson.com>
2012-06-12 15:48:13 +0800

    Achieved the diagnostic telnet logging for one IP address.

--- Tools/telnet_logging.rb
+++ Tools/telnet_logging.rb
@@ -3,27 +3,17 @@
 
 $:.unshift File.dirname(__FILE__).sub('Tools','lib') #add lib to load path
 require 'generic'
+$desired_location = '4,3'  # the guide of '4,3' can navigate to system operation info manu item,
 
-def get_input_parameters()
-  input_parameters = Hash.new()
-  if ARGV.length == 6
-    input_parameters = { "ip_address"=>ARGV[0], "user_name"=>ARGV[1], "password"=>ARGV[2], "interval_time"=>ARGV[3], "desired_location"=>ARGV[4],"output_file"=>ARGV[5]}
-  else
-    puts "Please type the IP Address which you want to telnet: "
-    input_parameters["ip_address"] = gets
-    puts "Please type the card's username which you want to telnet: "
-    input_parameters["user_name"] = gets
-    puts "Please type the card's password which you want to telnet: "
-    input_parameters["password"] = gets
-    puts "Please type the Interval Time: "
-    input_parameters["interval_time"] = gets.chomp.to_i
-    puts "Please type the desired location which you want to navigate, following the format as <n,n>, n should be integer number: "
-    input_parameters["desired_location"] = gets
-    puts "Please type the output File which you want to store the log: "
-    input_parameters["output_file"] = gets
-  end
-  return input_parameters
-end
+def get_input_parameters(work_sheet)
+    input_parameters = Hash.new()
+    input_parameters["ip_address"] = work_sheet.Range("A2")['Value']
+    input_parameters["user_name"] = work_sheet.Range("B2")['Value']
+    input_parameters["password"] = work_sheet.Range("C2")['Value']
+    input_parameters["interval_time"] = work_sheet.Range("D2")['Value']
+    input_parameters["loop_times"] = work_sheet.Range("E2")['Value']
+    return input_parameters
+ end
 
 def is_card_available(ip)
     reply_from = Regexp.new('Reply from')
@@ -34,71 +24,76 @@ def is_card_available(ip)
       return false
   end
 
-def navigate_to_location(telnet, navigate_str)
+def navigate_to_location(telnet, navigate_str,logging_file)
   navigation = navigate_str.split(',')
       navigation.each do |s| 
-      #telnet.cmd(s) {|c| print c}
-      telnet.write(s) {|c| print c}
-      telnet.waitfor(/\?>|press any key to exit|Hit/) {|c| print c}
+          telnet.write(s) {|c| print c}
+          log_info = telnet.waitfor(/\?>|press any key to exit|Hit/) {|c| print c}
+          if s == '3'
+              # delete the first character( it is the navigate number).
+              log_info = log_info[1,log_info.length]
+              logging_file.write(log_info)
+          end  
       end
 end
 
-# create a new method to connect the telnet, because the /lib/telnet.rb have a 5s sleep.
-def telnet_connect(ip,login,pswd,output_log)
-    prompt_type=/[ftpusr:~>]*\z/n
-    puts"ip,=#{ip},login=#{login},password=#{pswd}"
-    telnet = Net::Telnet.new('Host' => ip,'Prompt' =>prompt_type ,"Output_log"   => output_log  )
-
-    #The prompt should be the real prompt while you entered your system
-    telnet.cmd(''){|c| print c}
-    telnet.waitfor(/[Ll]ogin[: ]*\z/n) {|c| print c}
-    telnet.cmd(login) {|c| print c}
-    telnet.waitfor(/Password[: ]*\z/n) {|c| print c}
-    telnet.cmd(pswd) {|c| print c}
-
-    telnet.waitfor(/[>]/n) {|c| print c}
-    return telnet
-  end
+def add_timestamp(logging_file)
+  logging_file.puts '-------------------------------------------------'
+  logging_file.puts Time.now
+  logging_file.puts '-------------------------------------------------'
+end
 
 begin
   g = Generic.new
-  input_parameters = get_input_parameters()
-  logging_file = File.new(input_parameters["output_file"], "a+")
+  
+  excel_name = File.dirname(__FILE__) + '\\' + 'Telnet_diagnostic_template.xls'
+  setup = g.new_xls(excel_name,1)
+  spreat_sheet = setup[0]
+  work_book = setup[1]
+  work_sheet = setup[2]
+
+  # get all input parameters
+  input_parameters = get_input_parameters(work_sheet)
+  ip_address = input_parameters["ip_address"]
+  interval_time = input_parameters["interval_time"]
+  loop_times = input_parameters["loop_times"]
+  
+  # open the output file,
+  output_file_name = File.dirname(__FILE__) + '\\' + ip_address + '_' + Time.now.strftime("%m-%d_%H-%M-%S") + '.txt'
+  logging_file = File.new(output_file_name, "a+")
   logging_file.sync = true
   logging_file.binmode
 
-  ip_address = input_parameters["ip_address"]
-  interval_time = input_parameters["interval_time"]
-
-  while   1
+  while  ( loop_times.to_i > 0)
     # record the card's state, available or not?
     is_available = is_card_available(ip_address)
-    if is_available
-        logging_file.puts("\n#{ip_address} is available, please wait for connect... \n\n")
 
+    # add the time stamp at the beginning of log file.
+    add_timestamp(logging_file)
+
+    if is_available
         # connect to card from telnet
-        #telnet = g.telnet_connect(input_parameters["ip_address"], input_parameters["user_name"], input_parameters["password"],input_parameters["output_file"])
-        telnet = telnet_connect(input_parameters["ip_address"], input_parameters["user_name"], input_parameters["password"],input_parameters["output_file"])
+        telnet = g.telnet_connect(ip_address, input_parameters["user_name"], input_parameters["password"])
 
         # navigate to desired location
-        navigate_to_location(telnet, input_parameters["desired_location"])
+        navigate_to_location(telnet, $desired_location,logging_file)
 
         # shut up telnet
         telnet.close
-
+        
       else
-      logging_file.puts("#{ip_address} is not available, please try again...")
+      logging_file.puts("#{ip_address} is not available, please check the connection...")
     end
 
-    # wait for the interval
-    logging_file.puts("\n\nPlease wait for  #{interval_time} seconds to start another logging...\n\n")
-    logging_file.puts("\n\n==========================================\n\n")
     sleep (interval_time.to_i)
+    loop_times = loop_times.to_i - 1
   end
 
 rescue Exception => e
    puts "Telnet logging failed: #{e}\n\n"
    puts $@.to_s
 ensure
+  work_book.close
+  spreat_sheet.quit
   logging_file.close
 end
